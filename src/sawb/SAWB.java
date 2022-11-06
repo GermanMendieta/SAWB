3da1e0b258cf79cdec93aad8589dfe8cfdc4efa8
Particular <Particular@LAPTOP-GERMAN>
2022-11-06 18:17:31 -0300

    Base del proyecto, pocas cositas: Login Menu principal

---
+++ src/sawb/SAWB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sawb;
+
+/**
+ *
+ * @author Particular
+ */
+public class SAWB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventanas.Inicio.main(args);
+    }
+    
+}
